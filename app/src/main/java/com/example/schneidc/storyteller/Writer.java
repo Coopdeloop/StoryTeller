d6bc0a4f02a4dcdf422fb340b5ccd9a8cffb5e00
Joseph <taillessbump@gmail.com>
2016-01-07 15:31:00 -0600

    Changed names of activities and added some new activities for future use.

--- app/src/main/java/com/example/schneidc/storyteller/Writer.java
+++ app/src/main/java/com/example/schneidc/storyteller/Writer.java
@@ -4,4 +4,13 @@ package com.example.schneidc.storyteller;
  * Created by schneidc on 12/18/15.
  */
 public class Writer {
+    private String mUsername;
+
+    public Writer(String username) {
+        mUsername = username;
+    }
+
+    public String getUsername(){
+        return mUsername;
+    }
 }

424c776df6b5b840d66c7a3ebdff4941205a65cd
Cooper <CooperSchneider@outlook.com>
2016-01-25 09:28:09 -0600

    Ready to pull

--- app/src/main/java/com/example/schneidc/storyteller/Writer.java
+++ app/src/main/java/com/example/schneidc/storyteller/Writer.java
@@ -4,10 +4,7 @@ package com.example.schneidc.storyteller;
  * Created by schneidc on 12/18/15.
  */
 public class Writer {
-<<<<<<< HEAD
 
-
-=======
     private String mUsername;
 
     public Writer(String username) {
@@ -17,5 +14,5 @@ public class Writer {
     public String getUsername(){
         return mUsername;
     }
->>>>>>> d6bc0a4f02a4dcdf422fb340b5ccd9a8cffb5e00
+
 }

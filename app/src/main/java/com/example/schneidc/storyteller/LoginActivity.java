dc030c2da0ff350e1e5801d731aa25b6cd84c53d
Cooper <CooperSchneider@outlook.com>
2016-02-17 09:06:47 -0600

    Merge branch 'master' of https://github.com/Coopdeloop/StoryTeller
    # Conflicts:
    #	app/src/main/res/layout/activity_open.xml
    Added OnClickListener

--- app/src/main/java/com/example/schneidc/storyteller/LoginActivity.java
+++ app/src/main/java/com/example/schneidc/storyteller/LoginActivity.java
@@ -1,17 +1,40 @@
 package com.example.schneidc.storyteller;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class LoginActivity extends ActionBarActivity {
+
+    private EditText mUsernameText;
+    private EditText mPasswordText;
+    private Button mSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mUsernameText = (EditText)findViewById(R.id.username);
+        mPasswordText = (EditText)findViewById(R.id.password);
+        mSubmit = (Button)findViewById(R.id.login_button);
+
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, OpenActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 
